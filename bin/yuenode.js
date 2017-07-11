e67313738f4fb52124af1abaf3de08e6227a9a47
shilei <shilei@yuewen.com>
2017-07-11 18:24:10 +0800

    fix 命令行启动配置文件

--- bin/yuenode.js
+++ bin/yuenode.js
@@ -6,7 +6,7 @@ const yuenode = require('../index.js');
 let config = {};
 try {
     if (process.argv.length > 2) {
-        config = require(process.argv[2]);
+        config = require(path.resolve(process.cwd(), process.argv[2]));
     } else {
         config = require(path.join(process.cwd(), 'yuenode.config.js'));
     }
